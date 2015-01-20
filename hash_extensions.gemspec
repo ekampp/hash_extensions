78e93673d8a259460698934c954d20fcfcdb141a
Emil Kampp <emil@kampp.me>
2015-01-20 13:02:05 +0100

    Build and publish as gem

---
+++ hash_extensions.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = 'hash_extensions'
+  s.version     = '1.0.0'
+  s.date        = '2014-01-20'
+  s.summary     = "Some common, practical hash hash_extensions"
+  s.description = "Some common, practical hash hash_extensions"
+  s.authors     = ["Emil Kampp"]
+  s.email       = 'emil@kampp.me'
+  s.files       = ["lib/hash_extensions.rb"]
+  s.homepage    = 'https://github.com/ekampp/hash_extensions'
+  s.license     = 'MIT'
+
+  s.add_runtime_dependency 'activesupport', ['~> 4.2']
+end
---
+++ lib/hash_extensions.rb
@@ -0,0 +1,33 @@
+require 'active_support/all'
+
+class Hash
+  # Search for key path in hash
+  #
+  # == Examples
+  #
+  #   { a: { b: :c } }.dig(:a, :b) # => :c
+  #   { a: { 'b' => :c } }.dig(:a, :b) # => :c
+  #   { a: { b: :c } }.dig(:a, :c) # => nil
+  def dig(*path)
+    path.reduce(self) do |location, key|
+      location.respond_to?(:keys) ? location.stringify_keys[key.to_s] : nil
+    end
+  end
+
+  # Search the Hash deeply for all values matching the +key+.
+  #
+  # == Examples
+  #
+  #   { a: :b, c: { a: '1', d: :e } }.values_for('a') # => [:b, '1']
+  def values_for(key)
+    found_values = []
+    self.each do |k, v|
+      found_values << v if k.to_s == key.to_s
+      if v.is_a? Hash
+        found_values << v.values_for(key)
+      end
+    end
+
+    found_values.flatten.compact
+  end
+end
